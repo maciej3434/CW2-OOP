a2bdcce41b6493091aa997405c71fc388b45eb8b
Ben McManus <McManus-B8@ulster.ac.uk>
2025-12-21 21:30:34 +0000

    Created fields and sample admin method

---
+++ Admin.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_Class
+{
+    internal class Admin: User
+    {
+        private DateOnly loginDate;
+
+        public DateOnly LoginDate
+        {
+            get { return loginDate; }
+            set { loginDate = value; }
+        }
+        public Admin (DateOnly loginDate, int userId, string userName, string password, string email, string role) : base(userId, userName, password, email, role)
+        {
+            this.loginDate = loginDate;
+        }
+
+        public Admin(int userId, string userName, string password, string email, string role) : base(userId, userName, password, email, role)
+        {
+        }
+
+        public static Admin CreateSampleAdmin()
+        {
+            return new Admin(
+                userId: 1,
+                userName: "sampleAdmin",
+                password: "Password123!",
+                email: "Admin@example.com",
+                role: "Admin"
+            )
+            {
+                loginDate = DateOnly.FromDateTime(DateTime.Now)
+            };
+        }
+
+    }
+}
